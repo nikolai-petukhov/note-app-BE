91427d713f8b11e4a1203c537c2b8523e7a22973
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-12-14 19:41:41 +0300

    uibuild

--- index.js
+++ index.js
@@ -13,26 +13,6 @@ app.use(requestLogger);
 app.use(cors());
 app.use(express.static('build'));
 
-// const url = `mongodb+srv://nikolaipetukhov:111nikolaipetukhov111@cluster0.lbxz0cr.mongodb.net/noteApp?retryWrites=true&w=majority`;
-// mongoose.connect(url);
-
-// const noteSchema = new mongoose.Schema({
-//   content: String,
-//   date: Date,
-//   important: Boolean
-// });
-
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString();
-//     delete returnedObject._id;
-//     delete returnedObject.__v;
-//   }
-// });
-
-// const Note = mongoose.model('Note', noteSchema);
-
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
@@ -44,10 +24,6 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  // const id = +request.params.id; // Number(request.params.id)
-  // const note = notes.find((note) => note.id === id);
-
-  // note ? response.json(note) : response.status(404).end();
   Note.findById(request.params.id).then(note => {
     response.json(note);
   })
@@ -68,10 +44,6 @@ app.post("/api/notes", (request, response) => {
     note.save().then(saveNote => {
       response.json(saveNote);
     });
-
-    // notes.concat(note);
-    
-    // response.json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
