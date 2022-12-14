803971d5cfabe9790502547e940a7d3b78969ae7
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-12-14 19:51:36 +0300

    uibuild

--- index.js
+++ index.js
@@ -57,4 +57,4 @@ app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
-console.log(`Server running on port ${PORT}`);//
+console.log(`Server running on port ${PORT}`);
