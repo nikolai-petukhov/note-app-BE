c8c2e6112d708b6d8dd32911b80c6f87e7ffe633
nikolai-petukhov <nikolaipetukhov@gmai.com>
2023-01-14 22:29:19 +0300

    uibuild

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const { requestLogger, unknownEndpoint} = require('./middleware/requestLogger');
 
 const Note = require('./models/note');
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
-app.use(express.static('build'));
+
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
@@ -23,10 +24,12 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note);
-  })
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      note ? response.json(note) : response.status(404).end(); 
+    })
+    .catch(error => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
@@ -46,14 +49,40 @@ app.post("/api/notes", (request, response) => {
     });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = +request.params.id;
-  notes = notes.filter((note) => note.id !== id);
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error));
+});
 
-  response.status(204).end();
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updateNote => {
+      response.json(updateNote);
+    })
+    .catch(error => next(error));
 });
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'});
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
